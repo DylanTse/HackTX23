e0b5d33282b1332d04c7b4c51484be465716b52f
matthew1 <gengarodyssey@gmail.com>
2023-10-21 13:12:31 -0500

    block scroll thing

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,76 +1,87 @@
 import React, { useEffect, useState } from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF, InfoWindowF } from '@react-google-maps/api';
 
 const containerStyle = {
   display: 'flex',
   width: '100%',
-  height: '90vh',
+  height: '89vh',
 };
 
 const sidebarStyle = {
   width: '25%', // Adjust the width of the sidebar as needed
-  height: '100%',
-  overflowY: 'auto'
+  height: '89vh',
+  overflowY: 'auto', // Enable vertical scrolling
+  background: '#f0f0f0', // Added for better visibility
+};
+
+const blockStyle = {
+  height: '10vh',
 };
 
 const MapComponent = ({ userLocation }) => {
   const [selectedMarker, setSelectedMarker] = useState(null);
+  const [map, setMap] = useState(null);
 
   const onMarkerClick = (marker) => {
     setSelectedMarker(marker);
   };
 
+  const centerMapOnMarker = (position) => {
+    if (map) {
+      map.panTo(position);
+    }
+  };
+
+  const Block = ({ onClick }) => {
+    // const handleClick = () => {
+    //   // Assuming you have a specific position for this block
+    //   const blockPosition = { lat: 30.2850, lng: -97.7335 };
+    //   onClick(blockPosition);
+
+    return (
+      <div style={blockStyle}>
+        <p>Content for Block goes here...</p>
+      </div>
+    );
+    
+
+  }
   
-const Block = () => {
-  return (
-    <div>
-      <p>Content for Block goes here...</p>
-    </div>
-  );
-}
   return (
     <div style={{ display: 'flex' }}>
       <div style={sidebarStyle}>
         <h2>Sidebar</h2>
+        <Block onClick={centerMapOnMarker} />
         <Block/>
         <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
-        <Block/>
-        <Block/><Block/>
         <Block/>
         <Block/>
+        <Block/>
+        <Block/>       
+        <Block/>
+        <Block/>
+        <Block/>
+        <Block/>        
+        <Block/>
+        <Block/>
+        <Block/>
+        <Block/>
+
       </div>
       <LoadScript googleMapsApiKey="AIzaSyCCaE3R5a3E5V1Wcmh9UBsSbKzFFOxBB74">
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={userLocation}
           zoom={20}
+          onLoad={(map) => setMap(map)}
         >
-          <Marker
+          <MarkerF
             position={userLocation}
             onClick={() => onMarkerClick(userLocation)}
           />
 
           {selectedMarker && (
-            <InfoWindow
+            <InfoWindowF
               position={selectedMarker}
               onCloseClick={() => setSelectedMarker(null)}
             >
@@ -78,7 +89,7 @@ const Block = () => {
                 <h3>Marker Info</h3>
                 <p>Additional information about the marker.</p>
               </div>
-            </InfoWindow>
+            </InfoWindowF>
           )}
         </GoogleMap>
       </LoadScript>
