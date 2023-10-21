2387ee006e04432f0e7b7954c8c36d9c35f80bf2
unknown <alannaphung816@gmail.com>
2023-10-21 12:17:01 -0500

    sidebar pls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { GoogleMap, LoadScript, MarkerF, InfoWindowF } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
   height: '90vh',
+};
+
+const sidebarStyle = {
+  width: '25%', // Adjust the width of the sidebar as needed
+  height: '100vh',
+  overflowY: 'scroll',
 };
 
 const MapComponent = ({ userLocation }) => {
@@ -15,44 +21,53 @@ const MapComponent = ({ userLocation }) => {
   };
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCCaE3R5a3E5V1Wcmh9UBsSbKzFFOxBB74">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={userLocation}
-        zoom={20}
-      >
-        <MarkerF
-          position={userLocation}
-          onClick={() => onMarkerClick(userLocation)}
-        />
+    <div style={{ display: 'flex' }}>
+      <div style={sidebarStyle}>
+        {/* Sidebar content goes here */}
+        <h2>Sidebar</h2>
+        <p>Content goes here...</p>
+      </div>
+      <LoadScript googleMapsApiKey="YOUR_API_KEY">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={userLocation}
+          zoom={20}
+        >
+          <Marker
+            position={userLocation}
+            onClick={() => onMarkerClick(userLocation)}
+          />
 
-        {selectedMarker && (
-          <InfoWindowF
-            position={selectedMarker}
-            onCloseClick={() => setSelectedMarker(null)}
-          >
-            <div>
-              <h3>Marker Info</h3>
-              <p>Additional information about the marker.</p>
-            </div>
-          </InfoWindowF>
-        )}
-      </GoogleMap>
-    </LoadScript>
-  )
-}
+          {selectedMarker && (
+            <InfoWindow
+              position={selectedMarker}
+              onCloseClick={() => setSelectedMarker(null)}
+            >
+              <div>
+                <h3>Marker Info</h3>
+                <p>Additional information about the marker.</p>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+};
 
 function App() {
-  const [userLocation, setUserLocation] = useState({lat: 0, lng: 0});
+  const [userLocation, setUserLocation] = useState({ lat: 0, lng: 0 });
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const {latitude, longitude} = position.coords;
-      setUserLocation({lat: latitude, lng: longitude});
-    }, (error) => {
-      console.error("Error getting user location:", error);
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ lat: latitude, lng: longitude });
+      },
+      (error) => {
+        console.error('Error getting user location:', error);
+      }
+    );
   }, []);
-
 
   return (
     <div className="App">
