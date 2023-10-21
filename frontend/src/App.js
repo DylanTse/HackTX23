05ba8336fa0fb702f030b91fc44e9970bc8b5403
DylanTse <dylantse@utexas.edu>
2023-10-21 11:30:35 -0500

    working map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,41 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
 
 const containerStyle = {
   width: '100%', // This will make the map container take up 100% of its parent element's width.
   height: '90vh', // This will set the height to 50% of the viewport height.
 };
 
-const MapComponent = () => {
+const MapComponent = ({userLocation}) => {
   return (
     <LoadScript googleMapsApiKey="AIzaSyCCaE3R5a3E5V1Wcmh9UBsSbKzFFOxBB74">
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
+        center={userLocation}
+        zoom={15}
       />
     </LoadScript>
   )
 }
 
 function App() {
+  const [userLocation, setUserLocation] = useState({lat: 0, lng: 0});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const {latitude, longitude} = position.coords;
+      setUserLocation({lat: latitude, lng: longitude});
+    }, (error) => {
+      console.error("Error getting user location:", error);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>LAVISH LOO</h1>
-      <MapComponent />
+      <MapComponent userLocation={userLocation}/>
     </div>
   );
 }
